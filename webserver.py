7862c99bf072625574002874c6107c7261474d96
Alfonso Lu Vargas <khalim83co@gmail.com>
2016-11-30 22:34:34 -0500

    <ip_host>/: Se agregan templates 'base.hmtl' e 'index.html'

--- webserver.py
+++ webserver.py
@@ -1,15 +1,15 @@
-# Importa libreria
-from flask import Flask, request
+"""prueba de programación UNACIONAL."""
+from flask import Flask, render_template
 
 
-# Instancia la variable acipet con la constanten "__name__"
 app = Flask(__name__)
 
-# WRAP: se llama el metodo route con la url raiz y se retorna la funcion 'def index'
+
 @app.route('/')
 def index():
-    return 'Servicio flask corriendo...'
+    """Servicio flask en ejecución."""
+    return render_template('index.html')
 
-# se llama el metodo run que ejecuta el servidor en el puerto 5000
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
