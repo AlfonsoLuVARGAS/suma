0a9b769e206f58a6a02f0ea31a7ef712a6b0a580
Alfonso Luis Vargas <alfonso.vargas@accionese.com>
2016-12-02 15:17:07 -0500

    Se realiza la suma de los datos

--- webserver.py
+++ webserver.py
@@ -1,6 +1,7 @@
 """prueba de programación UNACIONAL."""
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask_wtf.csrf import CsrfProtect
+from helper import english_numbers
 
 import forms
 
@@ -13,11 +14,27 @@ csrf = CsrfProtect()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    """Envia formulario por metodo get valida en post."""
+    """Envia formulario por metodo get valida en post.
+    Esta función devuelve la suma de dos numeros en inglés."""
     suma_form = forms.sumaForm(request.form)
     if request.method == 'POST' and suma_form.validate():
-        print(suma_form.second_number.data)
-        print(suma_form.first_number.data)
+        numbers = english_numbers()
+        fi = suma_form.first_number.data
+        se = suma_form.second_number.data
+        pos = 0
+        for num in numbers:
+            num = num.lower()
+            if num == fi:
+                pos_fi = pos
+                print(pos)
+            if num == se:
+                pos_se = pos
+                print(pos)
+            pos = pos + 1
+        suma = pos_fi + pos_se
+        success_message = 'The sum is {}'.format(numbers[suma])
+        flash(success_message)
+        return render_template('index.html', form=suma_form)
     else:
         print("Error en formulario")
     return render_template('index.html', form=suma_form)
